87ab887fde88d1cdb9027e550dbf05caf741d518
ahmed omar <ahmedomar1997.aoo@gmail.com>
2023-04-27 16:16:58 +0200

    first commit

---
+++ lib/presentaion/view/aa.dart
@@ -0,0 +1,89 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class BouncingBallDemo extends StatefulWidget {
+  @override
+  State<BouncingBallDemo> createState() => _BouncingBallDemoState();
+}
+
+class _BouncingBallDemoState extends State<BouncingBallDemo> {
+  late double marginTop;
+  late double margin1;
+
+  void changePosition(Timer t) async {
+    setState(() {
+      marginTop = marginTop == 0 ? 100 : 0;
+    });
+  }
+  void changePosition1(Timer t) async {
+    setState(() {
+      margin1 = margin1 == 0 ? 100 : 0;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    marginTop = 0;
+    margin1 = 0;
+
+    Timer.periodic(const Duration(seconds: 1), changePosition);
+    Timer.periodic(const Duration(seconds: 1), changePosition1);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/BackHeader.png"),
+              fit: BoxFit.cover
+          ),
+        ),
+        child:  Container(
+          margin: EdgeInsets.only(bottom: marginTop),
+          child: Container(
+           child: Padding(
+             padding: const EdgeInsets.only( top: 300),
+             child: Image(
+               image: AssetImage("assets/basketball.png"),
+               height: 5,
+               width: 5,
+             ),
+           ),
+          ),
+        ),
+      )
+    );
+
+
+      // Container(
+      //   constraints: BoxConstraints.expand(),
+      //   decoration: const BoxDecoration(
+      //     image: DecorationImage(
+      //         image: AssetImage("assets/BackHeader.png"),
+      //         fit: BoxFit.cover,
+      //     ),
+      //   ),
+      // ),
+    // Container(
+    //   margin: EdgeInsets.only(top: marginTop),
+    //   child: Container(
+    //     decoration: const BoxDecoration(
+    //       shape: BoxShape.circle,
+    //       color: Colors.green,
+    //     ),
+    //     width: 40.0,
+    //     height: 40.0,
+    //   ),
+    // ),
+  }
+}
+
+
+
+
+
+
---
+++ lib/presentaion/view/splash.screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+
+      _counter++;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+
+    print('ready in 3...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('ready in 2...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('ready in 1...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('go!');
+    FlutterNativeSplash.remove();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
