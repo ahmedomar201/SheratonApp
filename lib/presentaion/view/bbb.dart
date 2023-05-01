9380b4c92dea97c11d08f04bf7284ee86b82e9da
ahmed omar <ahmedomar1997.aoo@gmail.com>
2023-05-01 17:18:54 +0300

    update2

--- lib/presentaion/view/bbb.dart
+++ lib/presentaion/view/bbb.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class BouncingBallDemo2 extends StatefulWidget {
@@ -10,7 +12,35 @@ class BouncingBallDemo2 extends StatefulWidget {
 class _BouncingBallDemo2State extends State<BouncingBallDemo2> {
 
   bool play=false;
+  @override
+  void initState() {
+    super.initState();
+    play = true; // تعيين play على القيمة الافتراضية
 
+    // استخدام Timer.periodic لتغيير قيمة play كل 2 ثانية
+    Timer.periodic(Duration(milliseconds: 2000), (timer) {
+      setState(() {
+        play = !play;
+      });
+    });
+  }
+
+  void initialization() async {
+    await Future.delayed(const Duration(seconds: 1));
+    print('go!');
+  }
+
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   // Set _isExpanded to true after a delay of 2 seconds
+  //   Future.delayed(Duration(milliseconds: 2), () {
+  //     setState(() {
+  //       play = true;
+  //     });
+  //   });
+  // }
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -28,13 +58,15 @@ class _BouncingBallDemo2State extends State<BouncingBallDemo2> {
               ),
 
             ),
+
             AnimatedPositioned(
-              curve: Curves.easeInOut,
+              curve: Curves.easeInOutBack,
+              // top: play ? 50 : 0,
               duration: Duration(milliseconds: 1200),
-              height: play?100:0,
+              height: play?230:0,
               left: 0,
               right: 0,
-              bottom: play ? MediaQuery.of(context).size.height/2:0,
+              bottom: play ? MediaQuery.of(context).size.height/6:0,
               child: Center(
                 child: Container(
                   constraints: BoxConstraints.expand(),
@@ -48,31 +80,12 @@ class _BouncingBallDemo2State extends State<BouncingBallDemo2> {
               ),
             ),
             AnimatedPositioned(
-              curve: Curves.easeInOut,
-              duration: Duration(milliseconds: 1200),
-              height: play?100:0,
+              curve: Curves.easeInOutBack,
+              duration: Duration(milliseconds: 1000),
+              height: play?230:0,
               left: 0,
               right: 0,
-              bottom: play ? MediaQuery.of(context).size.height/2:0,
-              child: Center(
-                child: Container(
-                  constraints: BoxConstraints.expand(),
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage("assets/FootBall.png"),
-                    ),
-                  ),
-
-                ),
-              ),
-            ),
-            AnimatedPositioned(
-              curve: Curves.easeInOut,
-              duration: Duration(milliseconds: 1200),
-              height: play?100:0,
-              left: 0,
-              right: 0,
-              bottom: play ? MediaQuery.of(context).size.height/2:0,
+              bottom: play ? MediaQuery.of(context).size.height/6:0,
               child: Center(
                 child: Container(
                   constraints: BoxConstraints.expand(),
@@ -85,6 +98,26 @@ class _BouncingBallDemo2State extends State<BouncingBallDemo2> {
                 ),
               ),
             ),
+            AnimatedPositioned(
+              curve: Curves.easeInOutBack,
+              duration: Duration(milliseconds: 800),
+              height: play?230:0,
+              left: 0,
+              right: 0,
+              bottom: play ? MediaQuery.of(context).size.height/6:0,
+              child: Center(
+                child: Container(
+                  constraints: BoxConstraints.expand(),
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/FootBall.png"),
+                    ),
+                  ),
+
+                ),
+              ),
+            ),
+
             Container(
                 alignment: Alignment.bottomCenter,
                 child: Image(
@@ -94,16 +127,18 @@ class _BouncingBallDemo2State extends State<BouncingBallDemo2> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(
-          Icons.play_arrow
-        ),
-        onPressed: (){
-          setState(() {
-            play=!play;
-          });
-        },
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   child: Icon(
+      //     Icons.play_arrow
+      //   ),
+      //   onPressed: (){
+      //     setState(() {
+      //       play=!play;
+      //     });
+      //   },
+      // ),
     );
   }
 }
+//"assets/tenisBall.png"
+//"assets/FootBall.png"
